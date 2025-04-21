dd144db1af03123b0e39b988bc04ba5f8d06d389
crotch-watch <naps_08_bay@icloud.com>
2025-04-21 12:47:30 +0530

    add: mocks for fs.writeFile and arguments

--- src/spies-mocks/src/util/io.spec.js
+++ src/spies-mocks/src/util/io.spec.js
@@ -1,9 +1,18 @@
 import { it, expect, vi } from "vitest"
 import { promises as fs } from "fs"
-
 import writeData from "./io"
 
 vi.mock("fs")
+vi.mock("path", () => {
+  return {
+    default: {
+      join: (...paths) => {
+        const provided_filename_arg = paths[paths.length - 1]
+        return provided_filename_arg
+      }
+    }
+  }
+})
 
 it("should mock node:fs and spy fs.writeFile invokation", () => {
   const test_filename = "./test.txt"
@@ -12,4 +21,24 @@ it("should mock node:fs and spy fs.writeFile invokation", () => {
   writeData(test_data, test_filename)
 
   expect(fs.writeFile).toBeCalled()
+})
+
+it("should check arguments passed to fs.writeFile", () => {
+  const test_filename = "./test.txt"
+  const test_data = "test data"
+
+  writeData(test_data, test_filename)
+
+  expect(fs.writeFile).toBeCalledWith(test_filename, test_data)
+  // there could also be one case where I can check whether
+  // path.join return includes test_filename
+})
+
+it("should resolve with undedined", async () => {
+  const test_filename = "./test.txt"
+  const test_data = "test data"
+
+  const result = await writeData(test_data, test_filename)
+
+  return expect(result).toBeUndefined()
 })
